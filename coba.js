168342554dad582fd4d9cdb3b61920623648f63b
JuanSeptian <juanseptian06@gmail.com>
2023-09-16 17:13:54 +0700

    First commit

---
+++ coba.js
@@ -0,0 +1,5 @@
+const skorPlayer = document.querySelector(".skorPlayer");
+const skorComputer = document.querySelector(".skorComputer");
+const skor = Math.round(Math.random() * 1);
+console.log(skor);
+console.log(skor);
